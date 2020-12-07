49e99ee2c0dfe02cdc074dc3771f987a1f0d5484
shdam <soren.helweg@gmail.com>
2020-12-07 08:14:03 +0100

    Simplify R folder

--- R/pData_ilaria.R
+++
@@ -1,8 +0,0 @@
-#' @title Phenotype data for new samples
-#' @source Kindly offered by Fred
-#' @description pData_ilaria records phenotype data corresponding to exprs_ilaria
-#' @docType data
-#' @usage pData_ilaria
-#' @format a data frame
-#' @keywords datasets
-"pData_ilaria"
